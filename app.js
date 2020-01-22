cbccadef7d7801fed7e5920957d5a4f56115bb68
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2020-01-22 11:14:00 +0100

    serving image file from nodejs, some bugs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var es6Renderer = require('express-es6-template-engine');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var editorsRouter = require('./routes/editors');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/editors', editorsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
