7d135f0dd8f13879b74aa83a59cb7b6f7f7adb71
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2020-01-24 08:32:29 +0100

    added csv parser

--- controllers/getCsv.js
+++ controllers/getCsv.js
@@ -1,24 +1,33 @@
 // code from https://github.com/aitchkhan/express-csv-download
 
 // package used to transform json to csv string
-const csvParse = require('csv-parse');
-const dataEditors = require('../data.csv');
+const csv = require('csv-parser');
+const fs = require('fs');
+// const dataEditors = require('../data.csv');
 
 module.exports = {
   downloadCsv
 };
 
 function downloadCsv(req, res) {
-  // adding appropriate headers, so browsers can start downloading
-  // file as soon as this request starts to get served
-  res.setHeader('Content-Type', 'text/csv');
-  res.setHeader('Content-Disposition', 'attachment; filename=\"' + 'download-' + Date.now() + '.csv\"');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Pragma', 'no-cache');
-
-// console.log(csvParse(dataEditors));
-  // stringify returns a readable stream that can be directly piped to a
-  // writeable stream which is "res" (the response object)
-  csvParse(dataEditors, { header: true })
-    .pipe(res);
+  fs.createReadStream("../data.csv")
+    .pipe(csv({separator: ","}))
+    .on("data", (row) => {
+      console.log(row);
+    })
+    .on("end", () => {
+      console.log("CSV file read");
+    });
+//   // adding appropriate headers, so browsers can start downloading
+//   // file as soon as this request starts to get served
+//   res.setHeader('Content-Type', 'text/csv');
+//   res.setHeader('Content-Disposition', 'attachment; filename=\"' + 'download-' + Date.now() + '.csv\"');
+//   res.setHeader('Cache-Control', 'no-cache');
+//   res.setHeader('Pragma', 'no-cache');
+//
+// // console.log(csvParse(dataEditors));
+//   // stringify returns a readable stream that can be directly piped to a
+//   // writeable stream which is "res" (the response object)
+//   csvParse(dataEditors, { header: true })
+//     .pipe(res);
 };
