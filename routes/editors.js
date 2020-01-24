7d33ea4c3116efb00a47c4547a08afa35982fa3c
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2020-01-24 12:17:27 +0100

    fixed csv file readings

--- routes/editors.js
+++ routes/editors.js
@@ -6,32 +6,35 @@ const csv = require('csv-parser');
 const fs = require('fs');
 
 // const dataEditors = require('../public/files/data_with_quotes.csv');
-// const dataEditors = require('data.csv');
+const dataEditors = './public/files/data.csv';
 
 /* GET . */
 // router.get('/csv', getCsvCtrl.downloadCsv);
 router.get('/csv', (req, res, next) => {
     // adding appropriate headers, so browsers can start downloading
     // file as soon as this request starts to get served
-    res.setHeader('Content-Type', 'text/csv');
-    res.setHeader('Content-Disposition', 'attachment; filename=\"' + 'download-' + Date.now() + '.csv\"');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Pragma', 'no-cache');
-
-    res.attachment('data.csv');
-
-    res.status(200).end();
+    // res.setHeader('Content-Type', 'text/csv');
+    // res.setHeader('Content-Disposition', 'attachment; filename=\"' + 'download-' + Date.now() + '.csv\"');
+    // res.setHeader('Cache-Control', 'no-cache');
+    // res.setHeader('Pragma', 'no-cache');
+    //
+    // res.attachment('data.csv');
+    //
+    // res.status(200).sendFile('data.csv');
 
     // res.download(dataEditors);
 
-  // fs.createReadStream('data_with_quotes.csv')
-  //   .pipe(csv({separator: ","}))
-  //   .on("data", (row) => {
-  //     console.log(row);
-  //   })
-  //   .on("end", () => {
-  //     console.log("CSV file read");
-  //   });
+  fs.createReadStream(dataEditors)
+    .on("error", (err) => {
+      console.log("error:" + err);
+    })
+    .pipe(csv({separator: ","}))
+    .on("data", (row) => {
+      console.log(row);
+    })
+    .on("end", () => {
+      console.log("CSV file read");
+    });
 
   // res.status(200).send(getCsvCtrl.downloadCsv);
 });
