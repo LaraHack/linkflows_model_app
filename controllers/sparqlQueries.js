2741bb52e6301998899bfb57eef451d0ed3898b8
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2020-01-28 16:25:59 +0100

    sparql queries request from client not yet working

--- controllers/sparqlQueries.js
+++ controllers/sparqlQueries.js
@@ -1,9 +1,7 @@
 // package used to transform json to csv string
 const csv = require('csv-parser');
 const fs = require('fs');
-const virtuosoClient = require('virtuoso-sparql-client');
-
-const dataEditors = './public/files/data.csv';
+const {Client, Node, Text, Data, Triple} = require('virtuoso-sparql-client');
 
 module.exports = {
   test
@@ -21,3 +19,36 @@ function test(req, res) {
       res.send(error);
     });
 };
+
+function testInsertion(res, req) {
+  const SaveClient = new Client("http://www.myendpoint.org/sparql");
+  SaveClient.setOptions(
+    "application/json",
+    {"myprefix": "http://www.myschema.org/ontology/"},
+    "http://www.myschema.org/resource/"
+  );
+
+  SaveClient.getLocalStore().add(
+    new Triple(
+      new Node("http://www.myschema.org/ontology/id123"),
+      "dcterms:created",
+      new Data(SaveClient.getLocalStore().now, "xsd:dateTimeStamp")
+    )
+  );
+  SaveClient.getLocalStore().add(
+    new Triple(
+      "myprefix:id123",
+      "rdfs:label",
+      new Text("A new lable", "en"),
+      Triple.ADD
+    )
+  );
+
+  SaveClient.store(true)
+  .then((result)=>{
+    console.log(result)
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
