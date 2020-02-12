2d59ec462ea49d4857e3124b08d20044d2bab9b5
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2020-02-12 16:51:09 +0100

    added checked template query for all (SPARQL endpoint still not working)

--- controllers/sparqlQueries.js
+++ controllers/sparqlQueries.js
@@ -11,8 +11,18 @@ module.exports = {
   buildQuery
 };
 
-var sparqlEndpoint1 = "http://dbpedia.org/sparql";
-var sparqlEndpoint2 = "http://localhost:8890/sparql";
+const sparqlEndpoint1 = "http://dbpedia.org/sparql";
+const sparqlEndpoint2 = "http://localhost:8890/sparql";
+
+const prefixes = {
+  doco: "http://purl.org/spar/doco/",
+  dcterms: "http://purl.org/dc/terms/",
+  po: "http://www.essepuntato.it/2008/12/pattern#",
+  prov: "http://www.w3.org/ns/prov#",
+  linkflows: "https://github.com/LaraHack/linkflows_model/blob/master/Linkflows.ttl#"
+};
+
+const articleTrustyURI = "http://purl.org/np/RAnVHrB5TSxLeOc6XTVafmd9hvosbs4c-4Ck0XRh_CgGk#articleVersion1";
 
 var testQuery1 = "DESCRIBE <http://dbpedia.org/resource/Sardinia>";
 var testQuery2 = "SELECT DISTINCT ?concept WHERE { ?s a ?concept .} LIMIT 50";
@@ -20,7 +30,6 @@ var testQuery3 = "SELECT (COUNT(*) as ?Triples) WHERE { ?s ?p ?o .}";
 var testQuery4 = "SELECT * WHERE { ?article a doco:Article . ?article dcterms:title ?title .}";
 
 function test(req, res) {
-  console.log("req: " + req);
   const SPARQLClient = new Client(sparqlEndpoint2);
 
   SPARQLClient.query(testQuery4)
@@ -38,78 +47,9 @@ function test(req, res) {
     });
 };
 
-// function testInsertion(res, req) {
-//   const SaveClient = new Client("http://www.myendpoint.org/sparql");
-//   SaveClient.setOptions(
-//     "application/json",
-//     {"myprefix": "http://www.myschema.org/ontology/"},
-//     "http://www.myschema.org/resource/"
-//   );
-//
-//   SaveClient.getLocalStore().add(
-//     new Triple(
-//       new Node("http://www.myschema.org/ontology/id123"),
-//       "dcterms:created",
-//       new Data(SaveClient.getLocalStore().now, "xsd:dateTimeStamp")
-//     )
-//   );
-//   SaveClient.getLocalStore().add(
-//     new Triple(
-//       "myprefix:id123",
-//       "rdfs:label",
-//       new Text("A new lable", "en"),
-//       Triple.ADD
-//     )
-//   );
-//
-//   SaveClient.store(true)
-//   .then((result)=>{
-//     console.log(result)
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-// }
-
-// var checkedDimensions = new Map([
-//   ["article", true],
-//   ["section", true],
-//   ["paragraph", true],
-//   ["syntax", true],
-//   ["style", true],
-//   ["content", true],
-//   ["negative", true],
-//   ["neutral", true],
-//   ["positive", true],
-//   ["I1", true],
-//   ["I2", true],
-//   ["I3", true],
-//   ["I4", true],
-//   ["I5", true],
-//   ["compulsory", true],
-//   ["suggestion", true],
-//   ["no_action", true]
-// ]);
-
-// Assumptions (checked checkboxes):
-//
-// Article level: section, paragraph
-// Aspect: syntax, content
-// Pos/Neg: negative, neutral, positive
-// Impact: 3, 4, 5
-// Action needed: compulsory
-
 function buildQuery(req, res) {
   //get values from checkboxes sent by client
-
-  // console.log("BODY REQ");
-  // console.log(req.query);
-  // console.log("/BODY REQ");
-
   var checkboxes = req.query;
-
-  // console.log("Checkboxes:");
-  // console.log(checkboxes);
 
   var article = (checkboxes.article == 'true');
   var section = (checkboxes.section == 'true');
@@ -147,79 +87,44 @@ function buildQuery(req, res) {
   console.log("suggestion:" + suggestion);
   console.log("no_action:" + no_action);
 
-
-  // const prefixes = {
-  //   doco: "http://purl.org/spar/doco/",
-  //   dcterms: "http://purl.org/dc/terms/",
-  //   po: "http://www.essepuntato.it/2008/12/pattern#",
-  //   prov: "http://www.w3.org/ns/prov#",
-  //   linkflows: "https://github.com/LaraHack/linkflows_model/blob/master/Linkflows.ttl#"
-  // };
-
   queryAll();
-
-  // var articleTrustyURI = "http://purl.org/np/RAnVHrB5TSxLeOc6XTVafmd9hvosbs4c-4Ck0XRh_CgGk#articleVersion1";
-  //
-  // var query = "SELECT ?reviewer ?reviewComment ?part ?aspect ?posNeg ?impact  ?actionNeeded " + "\n" +
-  // "WHERE { <" + articleTrustyURI + "> (po:contains)* ?part ." + "\n" +
-  //   "?reviewComment linkflows:refersTo ?part . " + "\n" +
-  //   "VALUES ?partType { doco:Article doco:Section doco:Paragraph } " + "\n" +
-  //   "?part a ?partType ." + "\n" +
-  //   "VALUES ?aspect { linkflows:SyntaxComment linkflows:StyleComment linkflows:ContentComment } " + "\n" +
-  //   "?reviewComment a ?aspect ." + "\n" +
-  //   "VALUES ?posNeg { linkflows:PositiveComment linkflows:NeutralComment linkflows:NegativeComment }" + "\n" +
-  //   "?reviewComment a ?posNeg ." + "\n" +
-  //   "?reviewComment linkflows:hasImpact ?impact ." + "\n" +
-  //   "FILTER (?impact = '1'^^xsd:positiveInteger || ?impact = '2'^^xsd:positiveInteger || ?impact = '3'^^xsd:positiveInteger || ?impact = '4'^^xsd:positiveInteger || ?impact = '5'^^xsd:positiveInteger) ." + "\n" +
-  //   "VALUES ?actionNeeded { linkflows:ActionNeededComment linkflows:SuggestionComment linkflows:NoActionNeededComment}" + "\n" +
-  //   "GRAPH ?assertion { ?reviewComment a linkflows:ReviewComment . }" + "\n" +
-  //   "?assertion prov:wasAttributedTo ?reviewer ." + "\n" +
-  // "} GROUP BY ?reviewer ?part ?aspect ?posNeg ?impact ?actionNeeded" + "\n" +
-  // "ORDER BY ?reviewer ?part ?aspect ?posNeg ?impact ?actionNeeded"
-
 }
 
 function buildPrefix(prefix, url) {
-  // console.log("________________________");
-  // console.log("PREFIX " + prefix + ": <" + url + ">" + "\n");
-  // console.log("________________________");
   return "PREFIX " + prefix + ": <" + url + ">" + "\n";
 }
 
-function queryAll() {
-  const prefixes = {
-    doco: "http://purl.org/spar/doco/",
-    dcterms: "http://purl.org/dc/terms/",
-    po: "http://www.essepuntato.it/2008/12/pattern#",
-    prov: "http://www.w3.org/ns/prov#",
-    linkflows: "https://github.com/LaraHack/linkflows_model/blob/master/Linkflows.ttl#"
-  };
-
+function buildPrefixes(prefixes) {
   var prefixesString = "";
 
   for (var prefix in prefixes) {
     prefixesString = prefixesString.concat(buildPrefix(prefix, prefixes[prefix]));
   }
 
-  //
-  // var articleTrustyURI = "http://purl.org/np/RAnVHrB5TSxLeOc6XTVafmd9hvosbs4c-4Ck0XRh_CgGk#articleVersion1";
-  //
-  // var query = "SELECT ?reviewer ?reviewComment ?part ?aspect ?posNeg ?impact  ?actionNeeded " + "\n" +
-  // "WHERE { <" + articleTrustyURI + "> (po:contains)* ?part ." + "\n" +
-  //   "?reviewComment linkflows:refersTo ?part . " + "\n" +
-  //   "VALUES ?partType { doco:Article doco:Section doco:Paragraph } " + "\n" +
-  //   "?part a ?partType ." + "\n" +
-  //   "VALUES ?aspect { linkflows:SyntaxComment linkflows:StyleComment linkflows:ContentComment } " + "\n" +
-  //   "?reviewComment a ?aspect ." + "\n" +
-  //   "VALUES ?posNeg { linkflows:PositiveComment linkflows:NeutralComment linkflows:NegativeComment }" + "\n" +
-  //   "?reviewComment a ?posNeg ." + "\n" +
-  //   "?reviewComment linkflows:hasImpact ?impact ." + "\n" +
-  //   "FILTER (?impact = '1'^^xsd:positiveInteger || ?impact = '2'^^xsd:positiveInteger || ?impact = '3'^^xsd:positiveInteger || ?impact = '4'^^xsd:positiveInteger || ?impact = '5'^^xsd:positiveInteger) ." + "\n" +
-  //   "VALUES ?actionNeeded { linkflows:ActionNeededComment linkflows:SuggestionComment linkflows:NoActionNeededComment}" + "\n" +
-  //   "GRAPH ?assertion { ?reviewComment a linkflows:ReviewComment . }" + "\n" +
-  //   "?assertion prov:wasAttributedTo ?reviewer ." + "\n" +
-  // "} GROUP BY ?reviewer ?part ?aspect ?posNeg ?impact ?actionNeeded" + "\n" +
-  // "ORDER BY ?reviewer ?part ?aspect ?posNeg ?impact ?actionNeeded"
-  //
-  // console.log(prefixesString + "\n" + query);
+  return prefixesString;
+}
+
+function queryAll() {
+  // add prefixes to the query
+  var queryPrefixes = buildPrefixes(prefixes);
+
+  // write query
+  var query = "SELECT ?reviewer ?reviewComment ?part ?aspect ?posNeg ?impact  ?actionNeeded " + "\n" +
+  "WHERE { <" + articleTrustyURI + "> (po:contains)* ?part ." + "\n" +
+    "?reviewComment linkflows:refersTo ?part . " + "\n" +
+    "VALUES ?partType { doco:Article doco:Section doco:Paragraph } " + "\n" +
+    "?part a ?partType ." + "\n" +
+    "VALUES ?aspect { linkflows:SyntaxComment linkflows:StyleComment linkflows:ContentComment } " + "\n" +
+    "?reviewComment a ?aspect ." + "\n" +
+    "VALUES ?posNeg { linkflows:PositiveComment linkflows:NeutralComment linkflows:NegativeComment }" + "\n" +
+    "?reviewComment a ?posNeg ." + "\n" +
+    "?reviewComment linkflows:hasImpact ?impact ." + "\n" +
+    "FILTER (?impact = '1'^^xsd:positiveInteger || ?impact = '2'^^xsd:positiveInteger || ?impact = '3'^^xsd:positiveInteger || ?impact = '4'^^xsd:positiveInteger || ?impact = '5'^^xsd:positiveInteger) ." + "\n" +
+    "VALUES ?actionNeeded { linkflows:ActionNeededComment linkflows:SuggestionComment linkflows:NoActionNeededComment}" + "\n" +
+    "GRAPH ?assertion { ?reviewComment a linkflows:ReviewComment . }" + "\n" +
+    "?assertion prov:wasAttributedTo ?reviewer ." + "\n" +
+  "} GROUP BY ?reviewer ?part ?aspect ?posNeg ?impact ?actionNeeded" + "\n" +
+  "ORDER BY ?reviewer ?part ?aspect ?posNeg ?impact ?actionNeeded"
+
+  console.log(queryPrefixes + query);
 }
